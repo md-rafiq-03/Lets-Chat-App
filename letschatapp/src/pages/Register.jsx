6442da40ca9c59dcd3aa85c3ab19b0f47b10ce0d
Ankit-11525 <ankitpathak11525@gmail.com>
2023-07-10 02:56:31 +0530

    end to end chatting setup done

--- letschatapp/src/pages/Register.jsx
+++ letschatapp/src/pages/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
   const navigate = useNavigate();
 
   const handleSubmit=async (e)=>{
-    e.preventDefault()
+    e.preventDefault();
     const displayName=e.target[0].value;
     const email=e.target[1].value;
     const password=e.target[2].value;
